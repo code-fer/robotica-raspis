9a8fffeae9a0002ef7cf88e2b58a0ed7e200c654
Fernando Calvo Casado <32304580+code-fer@users.noreply.github.com>
2025-02-25 10:18:59 +0100

    Esqueleto para la segunda práctica

---
+++ para.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#
+# https://www.dexterindustries.com/BrickPi/
+# https://github.com/DexterInd/BrickPi3
+#
+# Copyright (c) 2016 Dexter Industries
+# Released under the MIT license (http://choosealicense.com/licenses/mit/).
+# For more information, see https://github.com/DexterInd/BrickPi3/blob/master/LICENSE.md
+# Modified by R. Aragues / L. Montano 2023.
+#
+# This code is an example for stopping all motors and sensors
+# 
+
+from __future__ import print_function # use python 3 syntax but make it compatible with python 2
+from __future__ import division       #                           ''
+
+import time     # import the time library for the sleep function
+import brickpi3 # import the BrickPi3 drivers
+
+BP = brickpi3.BrickPi3() # Create an instance of the BrickPi3 class. BP will be the BrickPi3 object.
+
+print("EMPEZAMOS")
+time.sleep(0.5)
+BP.reset_all()        # Unconfigure the sensors, disable the motors, and restore the LED to the control of the BrickPi3 firmware.
+print("DONE")
